ae6345fc11629663371fb1eedd950e6eebf4e19f
Adrian Delgado <git@adrianjdelgado.com>
2023-07-22 01:25:59 -0500

    Add SEO metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
   import { LightSwitch } from "@skeletonlabs/skeleton";
+  import { page } from "$app/stores";
+
+  const title = "MatEs";
+  const description =
+    "Donde la lógica y la creatividad se encuentran. ¡Únete a nuestros talleres y grupos de estudio!";
+  const url = `https://${$page.url.host}`;
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={url} />
+  <meta property="og:title" content={title} />
+  <!-- <meta property="og:image" content="https://example.com/image.jpg" /> -->
+  <meta property="og:description" content={description} />
+  <meta property="og:locale" content="es_EC" />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:url" content={url} />
+</svelte:head>
 
 <div class="container mx-auto grid h-full place-items-center">
   <div class="space-y-5">
-    <h1 class="h1">Let's get cracking bones!</h1>
+    <h1 class="h1">{title}</h1>
     <p>Start by exploring:</p>
     <ul>
       <li>
